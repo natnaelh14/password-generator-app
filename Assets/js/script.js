0bf3f533c4fcefed60ddb8a2a839c6c196002ee1
Natnael Haile <natnaelhaile@Natnaels-MacBook-Pro.local>
2021-06-18 19:39:38 -0400

    daily commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,33 +1,12 @@
+//Select elements by CSS selectors and save it to a variable.
 var userPasswordLengthText = document.getElementById("user-number");
-
 var checkboxSelected = document.getElementsByClassName("checkbox-01");
-
-// var userPasswordLength = userNumber.value;
-
 var passwordText = document.querySelector("#password");
-
-
 var resetBtn = document.querySelector("#reset");
-resetBtn.addEventListener("click", function() {
-  passwordText.value = "";
-  userPasswordLengthText.value = "";
-  document.getElementById('error-01').textContent = '';
-  document.getElementById('error-02').textContent = '';
-  
-  for (i = 0; i < checkboxSelected.length; i++){
-    checkboxSelected[i].checked = false ;
-  }
-});
-
 var copyBtn = document.querySelector("#copy");
-copyBtn.addEventListener("click", function() {
-  passwordText.select();
-  document.execCommand('copy')
-});
-
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", generatePassword);
 
+//Created an object of all the character I am going to use.
 var characterSet = {
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowercase: "abcdefghijklmnopqrstuvwxyz",
@@ -35,56 +14,81 @@ var characterSet = {
   special: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
 };
 
+//Created an event listener for reset button, where it clears all variable value from previous function
+resetBtn.addEventListener("click", function () {
+  passwordText.value = "";
+  userPasswordLengthText.value = "";
+  document.getElementById("error-01").textContent = "";
+  document.getElementById("error-02").textContent = "";
+  for (i = 0; i < checkboxSelected.length; i++) {
+    checkboxSelected[i].checked = false;
+  }
+});
+
+//Created an event listener for copy button, where it copies the generated password value.
+copyBtn.addEventListener("click", function () {
+  passwordText.select();
+  document.execCommand("copy");
+});
+
+//When the generate password is clicked, it will run the generatePassword function.
+generateBtn.addEventListener("click", generatePassword);
+
+//Function clears all variable value from previous function, before the function is run every time.
+function clearContents() {
+  document.querySelector("#password").value = "";
+  password = "";
+  document.getElementById("error-01").textContent = "";
+  document.getElementById("error-02").textContent = "";
+}
 
 function generatePassword() {
   clearContents();
-
   var userCheckboxSelected = [];
-  if (userPasswordLengthText.value === '') {
-    document.getElementById('error-01').textContent = 'Please enter a password length number';
+
+  //Error check
+  if (userPasswordLengthText.value === "") {
+    document.getElementById("error-01").textContent =
+      "Please enter a password length number";
     return;
   }
-  
+
+  //The for loop will save the checked checkboxes in the userCheckboxSelected array.
   for (i = 0; i < checkboxSelected.length; i++) {
     if (checkboxSelected[i].checked) {
       userCheckboxSelected.push(checkboxSelected[i].value.toLowerCase());
     }
   }
-
-  if(userCheckboxSelected.length === 0) {
-    document.getElementById('error-02').textContent = 'Please select at least one character';
+  if (userCheckboxSelected.length === 0) {
+    document.getElementById("error-02").textContent =
+      "Please select at least one character";
     return;
   }
 
+  //Convert user's password length input into an integer and saves it in the userPasswordLength variable.
   var userPasswordLength = parseInt(userPasswordLengthText.value);
 
-  var isValidPasswordSize = userPasswordLength >= 8 && userPasswordLength <= 128;
-
+  //Error check
+  var isValidPasswordSize =
+    userPasswordLength >= 8 && userPasswordLength <= 128;
   if (!isValidPasswordSize) {
-    document.getElementById('error-01').textContent = 'Please enter a valid password length';
+    document.getElementById("error-01").textContent =
+      "Please enter a valid password length";
     return;
   }
-
-  passwordText.value = getPassword();
-  function clearContents() {
-    document.querySelector("#password").value = "";
-    password = "";
-    document.getElementById('error-01').textContent = '';
-    document.getElementById('error-02').textContent = '';
-
-  }
-
+  //getPassword loops through checked characters and randomly generates a password.
   function getPassword() {
     var password = "";
-    for (i = 0; i <= userPasswordLength; i++) {
+    for (i = 0; i < userPasswordLength; i++) {
       var selectedCharacterIndex = Math.floor(
         Math.random() * userCheckboxSelected.length
       );
-      var selectedCharacter = characterSet[userCheckboxSelected[selectedCharacterIndex]];
+      var selectedCharacter =
+        characterSet[userCheckboxSelected[selectedCharacterIndex]];
       var characterIndex = Math.floor(Math.random() * selectedCharacter.length);
       password += selectedCharacter[characterIndex];
     }
-
     return password;
   }
+  passwordText.value = getPassword();
 }
--- README.md
+++ README.md
@@ -1,96 +1,12 @@
 <<<<<<< HEAD
-# password-generator-app
-=======
-# 03 JavaScript: Password Generator
+Project Name: Password Generator Application
+GitHub Link: https://natnaelh14.github.io/password-generator-app/
 
-## Your Task
+In this project, i created a password generator application. Instead of using propmt() and alert() to interact with the user, I created input elements
+in the html. I also created elements in html to post error messages, instead of using the methods above. 
+I utilized event listeners to run the functions, when reset/copy/generate buttons are clicked.
 
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+I created an object that includes all the characters to be used. Then I created function that loops through for the length of the password and generates 
+a random password using only user's choice of characters.
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria plus the following:
-
-  * The homework should not produce any errors in the console when you inspect it using Chrome DevTools.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
->>>>>>> 07de1d9 (First commit)
+I appreciate any feedback. Thanks!
